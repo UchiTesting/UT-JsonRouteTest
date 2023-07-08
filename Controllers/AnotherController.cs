4f4140934935821fa2ca7a84559a09381fc9fa3e
UchiTesing <uchitesting57@gmail.com>
2023-07-08 13:01:13 +0200

    Add AnotherController

---
+++ Controllers/AnotherController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UT_JsonRouteTest.Controllers
+{
+	public class AnotherController : Controller
+	{
+		// GET: AnotherController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: AnotherController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: AnotherController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: AnotherController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: AnotherController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: AnotherController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: AnotherController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: AnotherController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ Models/Another.cs
@@ -0,0 +1,8 @@
+﻿namespace UT_JsonRouteTest.Models
+{
+	public class Another
+	{
+        public int Id { get; set; }
+        public String TextData { get; set; }
+    }
+}
