ea55df1997a0f4ea4dca1318fc7baa3944760437
UchiTesing <uchitesting57@gmail.com>
2023-07-08 11:55:05 +0200

    Create ASP .NET Core solution

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UT_JsonRouteTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
